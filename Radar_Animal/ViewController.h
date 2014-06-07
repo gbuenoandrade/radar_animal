ff71dee83beb1978cc178c26d675af9c775ce061
Guilherme Andrade <guilherme.andrade@tecsinapse.com.br>
2014-06-07 00:01:48 -0300

    brute force adding

---
+++ Radar_Animal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Radar_Animal
+//
+//  Created by Guilherme Andrade on 6/6/14.
+//  Copyright (c) 2014 Guilherme Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Radar_Animal/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Radar_Animal
+//
+//  Created by Guilherme Andrade on 6/6/14.
+//  Copyright (c) 2014 Guilherme Andrade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
